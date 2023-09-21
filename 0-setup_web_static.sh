b61bacb4373fa56581fa9410c29fece3c60de6d8
driiisdev <olayinkayakub01@gmail.com>
2023-09-21 22:54:48 +0100

    tryin ansible, alternative to fabric

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # installs nginx
-apt-get -y update
-apt-get -y install nginx
-sed -i "/listen \[::\]:80 default_server/ a\\\trewrite ^/redirect_me http://www.holbertonschool.com permanent;" /etc/nginx/sites-available/default
-sed -i "/listen \[::\]:80 default_server/ a\\\tadd_header X-Served-By \"\$HOSTNAME\";" /etc/nginx/sites-available/default
-sed -i "/redirect_me/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo sed -i '37i\rewrite ^/redirect_me http://driiis.tech permanent;' /etc/nginx/sites-available/default
+sudo sed -i '38i\error_page 404 /error_404.html;' /etc/nginx/sites-available/default
+sudo sed -i '/^http {/a \\tadd_header X-Served-By $hostname;' /etc/nginx/nginx.conf
 # echo "Ceci n'est pas une page" > /var/www/html/custom_404.html
-service nginx start
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Hello Holberton School!" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
-service nginx restart
+sudo service nginx start
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo echo "Holberton School" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
+sudo service nginx restart
 exit 0
