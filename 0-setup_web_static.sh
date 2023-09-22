7193f53626e42d6d438b136d475041d01e3cd735
driiisdev <olayinkayakub01@gmail.com>
2023-09-22 02:09:05 +0100

    deploy fixed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,9 +9,8 @@ sudo sed -i '/^http {/a \\tadd_header X-Served-By $hostname;' /etc/nginx/nginx.c
 sudo service nginx start
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo echo "Holberton School" > /data/web_static/releases/test/index.html
+sudo echo "This is a test" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
-exit 0
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,60 +1,40 @@
 #!/usr/bin/python3
-""" Does deployment"""
+"""
+ that distributes an archive to your web servers,
+ using the function do_deploy
 
-from fabric.api import *
-import os
+ run:
+ fab -f 2-do_deploy_web_static.py do_deploy:archive_path=
+ versions/web_static_20170315003959.tgz -i my_ssh_private_key -u
+ ubuntu
+"""
+from fabric.api import env, put, run
+import os.path
 
 env.hosts = ["54.175.225.218", "35.153.98.2"]
 env.user = "ubuntu"
+env.key_filename = '~/.ssh/school'
+# env.use_ssh_config
 
 
 def do_deploy(archive_path):
-    """ Deploys archive to servers"""
+    """ deploy to a web server """
     if not os.path.exists(archive_path):
         return False
-
-    results = []
-
-    res = put(archive_path, "/tmp")
-    results.append(res.succeeded)
-
-    basename = os.path.basename(archive_path)
-    if basename[-4:] == ".tgz":
-        name = basename[:-4]
-    newdir = "/data/web_static/releases/" + name
-    run("mkdir -p " + newdir)
-    run("tar -xzf /tmp/" + basename + " -C " + newdir)
-
-    run("rm /tmp/" + basename)
-    run("mv " + newdir + "/web_static/* " + newdir)
-    run("rm -rf " + newdir + "/web_static")
-    run("rm -rf /data/web_static/current")
-    run("ln -s " + newdir + " /data/web_static/current")
-
-    return True
-
-
-def do_deploy(archive_path):
-    """ Deploys archive to servers"""
-    if not os.path.exists(archive_path):
+    try:
+        file = archive_path.split("/")[-1]
+        name = file.split(".")[0]
+        path = "/data/web_static/releases/".format(name)
+        put(archive_path, "/tmp/")
+        run('sudo mkdir -p {}{}/'.format(path, name))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(file, path, name))
+        run('sudo rm /tmp/{}'.format(file))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, name))
+        run("sudo rm -rf {}{}/web_static".format(path, name))
+        run('sudo rm -rf /data/web_static/current')
+        run("sudo ln -s {}{}/ /data/web_static/current".format(path, name))
+        run('echo "New version deployed!"')
+        return True
+    except BaseException:
+        # run('echo "wahala!"')
         return False
-
-    results = []
-
-    res = put(archive_path, "/tmp")
-    results.append(res.succeeded)
-
-    basename = os.path.basename(archive_path)
-    if basename[-4:] == ".tgz":
-        name = basename[:-4]
-    newdir = "/data/web_static/releases/" + name
-    run("mkdir -p " + newdir)
-    run("tar -xzf /tmp/" + basename + " -C " + newdir)
-
-    run("rm /tmp/" + basename)
-    run("mv " + newdir + "/web_static/* " + newdir)
-    run("rm -rf " + newdir + "/web_static")
-    run("rm -rf /data/web_static/current")
-    run("ln -s " + newdir + " /data/web_static/current")
-
-    return True
